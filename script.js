ae0c045725cd9a61533b29d5642af1dfcf9460af
rashedulislam05 <mdrashedulislamrabbi@gmail.com>
2025-11-20 21:53:47 +0600

    update script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+<script>
+window.addEventListener("scroll", function(){
+    const header = document.getElementById("header");
+    header.classList.toggle("sticky", window.scrollY > 50);
+});
+</script>
